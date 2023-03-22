abbfbbcbb8a80b14e986605dc060ff2a8e61f5d3
imdigitalashish <imdigitalashish@gmail.com>
2023-03-22 23:44:17 +0530

    Added Card Animations

--- frontend/src/app/components/stats-banner/stats-banner.component.ts
+++ frontend/src/app/components/stats-banner/stats-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-stats-banner',
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class StatsBannerComponent {
 
+
+
+  observer: IntersectionObserver;
+
+  constructor() {
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show")
+        }
+      })
+    });
+  }
+
+  @ViewChild("listOfCards") listOfCards: ElementRef;
+
+  ngAfterViewInit() {
+
+    document.querySelectorAll(".card").forEach((card) => {
+      console.log(card)
+      this.observer.observe(card);
+    })
+
+  }
 }
